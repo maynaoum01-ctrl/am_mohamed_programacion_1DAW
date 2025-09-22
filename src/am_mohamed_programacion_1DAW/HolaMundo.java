d342d00d945eb4d8ae7c9cef03d39a59b7fd3974
Mohammed <maynaoum01@educarex.es>
2025-09-22 10:14:22 +0200

    Ya he conectado con GitHub

--- src/am_mohamed_programacion_1DAW/HolaMundo.java
+++ src/am_mohamed_programacion_1DAW/HolaMundo.java
@@ -5,6 +5,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo!!!");
+		System.out.println("Repositorio conectado.!");
+
 	}
 
 }
