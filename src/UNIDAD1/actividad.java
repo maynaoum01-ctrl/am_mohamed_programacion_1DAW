a99c2acc20909095cdd3319146f3ca620038939e
Mohamed Aynaou Moutawakkil <maynaoum01@educarex.es>
2025-09-25 10:38:29 +0200

    actividad vaiable

--- src/UNIDAD1/actividad.java
+++ src/UNIDAD1/actividad.java
@@ -2,8 +2,8 @@ package UNIDAD1;
 public class actividad {
  public static void main(String[] args) {
  // Enteros
-int numero = 10;
-numero = "Hola";
+//int numero = 10;
+//numero = "Hola";
  long poblacion = 77000000L;
  // Decimales
  float precio = 8.98f;
@@ -12,12 +12,22 @@ numero = "Hola";
  char inicial = 'A';
  // Booleano
  boolean aprobado = true;
- System.out.println("Edad: " + edad);
+ 
+float siniva=100.0f;
+float iva=0.21f;
+float resultado = siniva * (1+ iva);
+
+ 
+ 
+ 
+ //System.out.println("Edad: " + edad);
  System.out.println("Población: " + poblacion);
  System.out.println("Precio: " + precio);
  System.out.println("PI: " + pi);
  System.out.println("Inicial: " + inicial);
  System.out.println("¿Aprobado? " + aprobado);
+ System.out.println("resultado: " + resultado);
+ 
  }
 }
 
